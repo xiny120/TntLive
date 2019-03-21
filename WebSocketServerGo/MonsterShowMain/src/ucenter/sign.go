04501a7daab8baff8be99b4e2dcf306963d7c5d9
毛毛 <20774746@qq.com>
2019-03-21 16:56:37 +0800

    单独地
    单独地

--- WebSocketServerGo/MonsterShowMain/src/ucenter/sign.go
+++ WebSocketServerGo/MonsterShowMain/src/ucenter/sign.go
@@ -1,12 +1,13 @@
 package sign
 
 import (
-	//"cfg"
-	"database/sql"
+	"cfg"
+	//"database/sql"
+
 	//"encoding/json"
 	//"log"
 	"fmt"
-	"strings"
+	//"strings"
 
 	//"github.com/satori/go.uuid"
 
@@ -27,46 +28,13 @@ const (
 	StaSigninSqlServerError
 )
 
-type Mssql struct {
-	*sql.DB
-	dataSource string
-	database   string
-	windows    bool
-	sa         SA
-}
-
-type SA struct {
-	user   string
-	passwd string
-}
-
-func (m *Mssql) Open() (err error) {
-	var conf []string
-	conf = append(conf, "Provider=SQLOLEDB")
-	conf = append(conf, "Data Source="+m.dataSource)
-	if m.windows {
-		// Integrated Security=SSPI 这个表示以当前WINDOWS系统用户身去登录SQL SERVER服务器(需要在安装sqlserver时候设置)，
-		// 如果SQL SERVER服务器不支持这种方式登录时，就会出错。
-		conf = append(conf, "integrated security=SSPI")
-	}
-	conf = append(conf, "Initial Catalog="+m.database)
-	conf = append(conf, "user id="+m.sa.user)
-	conf = append(conf, "password="+m.sa.passwd)
-
-	m.DB, err = sql.Open("adodb", strings.Join(conf, ";"))
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
 func SignIn(un string, pwd string) int {
-	db := Mssql{
+	db := cfg.Mssql{
 		dataSource: "hds12204021.my3w.com",
 		database:   "hds12204021_db",
 		// windwos: true 为windows身份验证，false 必须设置sa账号和密码
 		windows: false,
-		sa: SA{
+		sa: cfg.SA{
 			user:   "hds12204021",
 			passwd: "",
 		},
