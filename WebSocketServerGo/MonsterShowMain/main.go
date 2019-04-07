40e7e8751686f94dfd90f96d3ba1f0448e25e053
xiny120 <40488908+xiny120@users.noreply.github.com>
2019-04-08 00:16:40 +0800

    啊啊啊
    啊啊啊

--- WebSocketServerGo/MonsterShowMain/main.go
+++ WebSocketServerGo/MonsterShowMain/main.go
@@ -20,7 +20,8 @@ func main() {
 	initDB() //初始化数据库
 	flag.Parse()
 	r := mux.NewRouter()
-	r.HandleFunc("/", serveHome)
+	//r.HandleFunc("/admin", serveHome)
+	r.PathPrefix("/admin").Handler(http.StripPrefix("/admin", http.FileServer(http.Dir("static/"))))
 	r.HandleFunc("/ws/{id}", serveWebSocket)             // websocket的handleFunc.其中表示聊天室编号。
 	r.HandleFunc("/api/1.00/private", webapi100.Private) // webapi 1.0.0 协议
 	r.HandleFunc("/api/1.00/public", webapi100.Public)
@@ -33,13 +34,18 @@ func main() {
 }
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
-	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	http.ServeFile(w, r, "home.html")
+	//if r.URL.Path != "/" {
+	//	http.Error(w, "Not found", http.StatusNotFound)
+	//	return
+	//}
+	//if r.Method != "GET" {
+	//	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	//	return
+	//}
+
+	file := "static" + r.URL.Path
+
+	log.Println(file)
+
+	http.ServeFile(w, r, file)
 }
